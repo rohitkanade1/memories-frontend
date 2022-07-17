7eb40a6c195550ad6b53935f32fff0ff976c99bf
unknown <rohitkanademx@gmail.com>
2022-07-17 20:16:31 +0530

    Added component folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
 const App = () => {
-    return(<div>
-        <h1>APP</h1>
-    </div>)
+  return (
+    <div>
+      <h1>APP</h1>
+    </div>
+  );
 };
-
 
 export default App;
